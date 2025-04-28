7c3722b4e821e468a8692c7a08f0383b37f35a42
Petr Melnikov <petr.melnikov.a@gmail.com>
2025-04-28 23:13:03 +0700

    simple_http_req

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+//https://developers.binance.com/docs/binance-spot-api-docs/rest-api/market-data-endpoints
+// {
+//     "lastUpdateId": 1027024,
+//     "bids": [
+//       [
+//         "4.00000000",     // PRICE
+//         "431.00000000"    // QTY
+//       ]
+//     ],
+//     "asks": [
+//       [
+//         "4.00000200",
+//         "12.00000000"
+//       ]
+//     ]
+//   }
+
+
+// This is using the `tokio` runtime. You'll need the following dependency:
+//
+// `tokio = { version = "1", features = ["full"] }`
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    // Some simple CLI args requirements...
+    let body = reqwest::get("https://data-api.binance.vision/api/v3/depth?symbol=BNBBTC&limit=3")
+    .await?
+    .text()
+    .await?;
+
+    println!("body = {body:?}");
+
+    Ok(())
 }
