1730b627ee9ca61bd52ebc507098c4bc5a741b8a
Petr Melnikov <petr.melnikov.a@gmail.com>
2025-05-10 17:40:32 +0700

    update_from_imple

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,64 @@ use json_parser::json_helper::PriceLevelsSnapshot;
 pub use ws_connector::ws_connector_impl;
 
 
-
+#[derive(Debug)]
 pub struct PriceLevels {
     last_update_id: i64,
     bids: std::collections::BTreeMap<String, String>,
     asks: std::collections::BTreeMap<String, String>
 }
 
+impl PriceLevels {
+
+    fn update_from_incremental (&mut self, inc_upd: json_helper::PriceLevelsIncremental) {
+        if (inc_upd.u < self.last_update_id) {
+            // Nothing to do
+            return;
+        }
+
+        if (inc_upd.U > self.last_update_id) {
+            !panic!("Something went wrong");
+        }
+
+        self.last_update_id = inc_upd.u;
+        
+        //TODO make closure
+        for ask in inc_upd.a {
+            let parsed: f32 = ask.1.parse().unwrap();
+            //TODO comp double with 0
+            if parsed == 0.0 {
+                self.asks.remove(ask.0.to_string().as_str());
+                continue;
+            }
+
+            self.asks.insert(ask.0, ask.1);
+        }
+
+        // for (bid in inc_upd.b) {
+        //     if bid[1] == 0 {
+        //         self.bids.remove(bid[0]);
+        //         continue;
+        //     }
+
+        //     self.bids.insert(ask[0], ask[1]);
+        // }
+
+    }
+
+
+}
+
+
 use std::{collections::BTreeMap, io::{stdout, Write}};
 
 async fn get_first_snapshot(first_increment_id : i64) -> PriceLevelsSnapshot {
-    for i in (1..5) {
+    for i in 1..6 {
         print!("attempt {i}");
         let snapshot_string = get_start_snapshot_from("https://data-api.binance.vision/api/v3/depth?symbol=BNBBTC&limit=100").await.unwrap();
         let snapshot = json_helper::parse_snapshot(&snapshot_string).unwrap();
         let snapshot_id = snapshot.lastUpdateId;
 
         if (snapshot_id >= first_increment_id) {
-            //TODO now i don't know how to make initialisation for ws_connection properly
             return snapshot;
         }
     }
@@ -52,7 +92,17 @@ fn main()  {
         let asks1 = BTreeMap::from_iter(snapshot.asks);
         let mut price_levels : PriceLevels  = PriceLevels{last_update_id: snapshot.lastUpdateId, bids: bids1, asks : asks1  };
         
-        //println!({price_levels:?});
+        println!("{price_levels:?}");
+
+        price_levels.update_from_incremental(first_incremental);
+
+        println!("{price_levels:?}");
+
+        // let fut1 = async {
+            
+        // };
+
+
 
 
         //     let jopa = self.stream.next().await;
