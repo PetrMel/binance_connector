418fd45b2282d29f4ab53e80ddc69023767de08d
Petr Melnikov <petr.melnikov.a@gmail.com>
2025-05-10 14:22:57 +0700

    init_snapshot

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod ws_connector;
 pub use http_connector::get_request;
 use http_connector::get_request::get_start_snapshot_from;
 pub use json_parser::json_helper;
+use json_parser::json_helper::PriceLevelsSnapshot;
 pub use ws_connector::ws_connector_impl;
 
 
@@ -16,6 +17,23 @@ pub struct PriceLevels {
 }
 
 use std::{collections::BTreeMap, io::{stdout, Write}};
+
+async fn get_first_snapshot(first_increment_id : i64) -> PriceLevelsSnapshot {
+    for i in (1..5) {
+        print!("attempt {i}");
+        let snapshot_string = get_start_snapshot_from("https://data-api.binance.vision/api/v3/depth?symbol=BNBBTC&limit=100").await.unwrap();
+        let snapshot = json_helper::parse_snapshot(&snapshot_string).unwrap();
+        let snapshot_id = snapshot.lastUpdateId;
+
+        if (snapshot_id >= first_increment_id) {
+            //TODO now i don't know how to make initialisation for ws_connection properly
+            return snapshot;
+        }
+    }
+    !panic!();
+
+}
+
 
 fn main()  {
     trpl::run(async {
@@ -29,18 +47,10 @@ fn main()  {
         let first_incremental = json_helper::parse_incremental(message.to_text().unwrap()).unwrap();
         let first_increment_id: i64 = first_incremental.U;
 
-        let snapshot_string = get_start_snapshot_from("https://data-api.binance.vision/api/v3/depth?symbol=BNBBTC&limit=100").await.unwrap();
-        let snapshot = json_helper::parse_snapshot(&snapshot_string).unwrap();
-        let snapshot_id = snapshot.lastUpdateId;
-
-        if (first_increment_id < snapshot_id) {
-            //TODO now i don't know how to make initialisation for ws_connection properly
-            !panic!();
-        }
-
+        let snapshot = get_first_snapshot(first_increment_id).await;
         let bids1 = BTreeMap::from_iter(snapshot.bids);
         let asks1 = BTreeMap::from_iter(snapshot.asks);
-        let mut price_levels : PriceLevels  = PriceLevels{last_update_id: snapshot_id, bids: bids1, asks : asks1  };
+        let mut price_levels : PriceLevels  = PriceLevels{last_update_id: snapshot.lastUpdateId, bids: bids1, asks : asks1  };
         
         //println!({price_levels:?});
 
